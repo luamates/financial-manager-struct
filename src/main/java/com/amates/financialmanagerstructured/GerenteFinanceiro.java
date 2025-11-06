2b53dc7288a2f0f7db63858759d394e9c672cc8c
luanates <lucas.amates@gmail.com>
2025-11-06 13:34:33 -0300

    Definicao de metodos e parametros

--- src/main/java/com/amates/financialmanagerstructured/GerenteFinanceiro.java
+++ src/main/java/com/amates/financialmanagerstructured/GerenteFinanceiro.java
@@ -1,35 +1,66 @@
 package com.amates.financialmanagerstructured;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class GerenteFinanceiro {
 
-
-
-
-
-
+    static ArrayList<String> types = new ArrayList<>();
+    static ArrayList<String> categories = new ArrayList<>();
+    static ArrayList<String> descriptions = new ArrayList<>();
+    static ArrayList<Double> values = new ArrayList<>();
+    static ArrayList<String> dates = new ArrayList<>();
+    static int maxTsc = 0;
 
     static void main() {
 
+        Scanner scanner = new Scanner(System.in);
 
+
+    }
+
+    public static void mainMenu(Scanner scanner) {
+
+
+    }
+
+    public static void addIncome(Scanner scanner) {
 
 
     }
 
 
+    public static void addExpense(Scanner scanner) {
 
 
+    }
+
+    public static void getBalance(Scanner scanner) {
 
 
+    }
+
+    public static void addToHistory(Scanner scanner) {
 
 
+    }
 
 
+    public static void getHistory(Scanner scanner) {
 
 
+    }
 
 
+    public static void getCategorySummary(Scanner scanner) {
 
 
+    }
+
+    public static void generateReport() {
+
+
+    }
 
 
 }
