b7e200ac863785ffd5d803e1f5ba92aa1f9ac0bb
luanates <lucas.amates@gmail.com>
2025-11-06 14:05:29 -0300

    Implantacao metodo addToHistory

--- src/main/java/com/amates/financialmanagerstructured/GerenteFinanceiro.java
+++ src/main/java/com/amates/financialmanagerstructured/GerenteFinanceiro.java
@@ -10,18 +10,59 @@ public class GerenteFinanceiro {
     static ArrayList<String> descriptions = new ArrayList<>();
     static ArrayList<Double> values = new ArrayList<>();
     static ArrayList<String> dates = new ArrayList<>();
+    static ArrayList<String> history = new ArrayList<>();
     static int maxTsc = 0;
 
     static void main() {
-
         Scanner scanner = new Scanner(System.in);
+        boolean continueMenu = true;
 
-
+        while (continueMenu) {
+            continueMenu = mainMenu(scanner);
+        }
+        scanner.close();
     }
 
-    public static void mainMenu(Scanner scanner) {
+    public static boolean mainMenu(Scanner scanner) {
 
+        System.out.printf("%n%n===== GERENTE FINANCEIRO =====%n");
+        System.out.printf("1 - Adicionar Receita%n");
+        System.out.printf("2 - Adicionar Despesa%n");
+        System.out.printf("3 - Consultar Saldo Atual%n");
+        System.out.printf("4 - Consultar Histórico de Transações%n");
+        System.out.printf("5 - Consultar Total por Categoria%n");
+        System.out.printf("6 - Gerar relatório em arquivo%n");
+        System.out.printf("7 - Sair%n");
+        System.out.printf("%nSua seleção: ");
 
+        int userInput = scanner.nextInt();
+
+        switch (userInput) {
+            case 1:
+                addIncome(scanner);
+                break;
+            case 2:
+                addExpense(scanner);
+                break;
+            case 3:
+                getBalance(scanner);
+                break;
+            case 4:
+                getHistory(scanner);
+                break;
+            case 5:
+                getCategorySummary(scanner);
+                break;
+            case 6:
+                generateReport();
+                break;
+            case 7:
+                return false;
+            default:
+                System.out.printf("%n%nOPÇÃO INVÁLIDA!");
+                break;
+        }
+        return true;
     }
 
     public static void addIncome(Scanner scanner) {
@@ -40,9 +81,8 @@ public class GerenteFinanceiro {
 
     }
 
-    public static void addToHistory(Scanner scanner) {
-
-
+    public static void addToHistory(String date, String type, String category, double value, String description) {
+        history.add("[" + date + "] " + type + " | " + category + " | " + value + " | " + description + "%n");
     }
 
 
